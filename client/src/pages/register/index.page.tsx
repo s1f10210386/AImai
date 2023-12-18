9141258e956e42f2f723740fa4fa08dd20940e1c
s1f10210386 <s1f102103864@iniad.org>
2023-12-18 17:33:10 +0900

    エラーなおせた

---
+++ client/src/pages/register/index.page.tsx
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <div>新規登録</div>;
+};
+
+export default Register;
--- client/src/utils/login.ts
+++ client/src/utils/login.ts
@@ -1,31 +1,12 @@
-import { signInWithEmailAndPassword } from 'firebase/auth';
-import { useRouter } from 'next/navigation';
-import type { SubmitHandler } from 'react-hook-form';
-import { auth } from './firebase';
-
-type FormValues = {
-  email: string;
-  password: string;
-};
-export const signInWithEmail = () => {
-  const router = useRouter();
-  const onSubmit: SubmitHandler<FormValues> = async (data) => {
-    console.log('data', data);
-    await signInWithEmailAndPassword(auth, data.email, data.password).then((userCredentiall) => {
-      router.push('/');
-    });
-  };
-};
-// import { GithubAuthProvider, signInWithPopup } from 'firebase/auth';
+import { GithubAuthProvider } from 'firebase/auth';
 // import { createAuth } from 'src/utils/firebase';
-// import { returnNull } from './returnNull';
 
-// export const loginWithGitHub = async () => {
-//   const ghProvider = new GithubAuthProvider();
-//   ghProvider.addScope('read:user');
+export const loginWithGitHub = async () => {
+  const ghProvider = new GithubAuthProvider();
+  ghProvider.addScope('read:user');
 
-//   await signInWithPopup(createAuth(), ghProvider).catch(returnNull);
-// };
+  // await signInWithPopup(createAuth(), ghProvider).catch(returnNull);
+};
 
 // export const logout = async () => {
 //   await createAuth().signOut();
