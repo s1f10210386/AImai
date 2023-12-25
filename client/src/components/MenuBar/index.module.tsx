e0e1a75e354573603a7dfb773f98c92fc1628131
s1f10210386 <s1f102103864@iniad.org>
2023-12-25 11:40:09 +0900

    menu Bar

--- client/src/components/MenuBar/index.module.tsx
+++ client/src/components/MenuBar/index.module.tsx
@@ -1,9 +1,18 @@
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { IconButton } from '@mui/material';
 import styles from './index.module.css';
-
 const MenuBar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.roomName}>Menu</div>
+      <div className={styles.button} />
+      <IconButton color="primary" sx={{ marginLeft: '10px' }}>
+        <LogoutIcon sx={{ fontSize: '30px' }} />
+      </IconButton>
+
+      <IconButton color="primary" sx={{ marginLeft: 'auto', marginRight: '10px' }}>
+        <CalendarMonthIcon sx={{ fontSize: '30px' }} />
+      </IconButton>
     </div>
   );
 };
