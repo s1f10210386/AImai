5f72db7173e747893492983c1228ca5fca930db5
s1f10210386 <s1f102103864@iniad.org>
2023-12-25 11:48:20 +0900

    logout

--- client/src/components/MenuBar/index.module.tsx
+++ client/src/components/MenuBar/index.module.tsx
@@ -1,17 +1,32 @@
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import LogoutIcon from '@mui/icons-material/Logout';
+import SettingsIcon from '@mui/icons-material/Settings';
 import { IconButton } from '@mui/material';
+import { useRouter } from 'next/navigation';
+import { auth } from 'src/utils/firebase';
 import styles from './index.module.css';
+
 const MenuBar = () => {
+  const router = useRouter();
+
+  const logout = () => {
+    auth.signOut();
+    router.push('/login');
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.button} />
-      <IconButton color="primary" sx={{ marginLeft: '10px' }}>
+      <IconButton color="primary" sx={{ marginLeft: '5px' }} onClick={() => logout()}>
         <LogoutIcon sx={{ fontSize: '30px' }} />
       </IconButton>
 
-      <IconButton color="primary" sx={{ marginLeft: 'auto', marginRight: '10px' }}>
+      <IconButton color="primary" sx={{ marginLeft: 'auto' }}>
         <CalendarMonthIcon sx={{ fontSize: '30px' }} />
+      </IconButton>
+
+      <IconButton color="primary" sx={{ marginRight: '5px' }}>
+        <SettingsIcon sx={{ fontSize: '30px' }} />
       </IconButton>
     </div>
   );
