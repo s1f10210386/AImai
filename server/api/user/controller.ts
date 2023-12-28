3d1ecc89ec56452d389e06c86d07731b7d84eb3d
金澤帆高 <hotaka@Pro.local>
2023-12-28 17:14:12 +0900

    aa

--- server/api/user/controller.ts
+++ server/api/user/controller.ts
@@ -2,6 +2,6 @@ import { createUser } from '$/repository/userRepository';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  // get: () => ({ status: 200, body: 'Hello' }),
+  get: () => ({ status: 200, body: 'Hello' }),
   post: async ({ body }) => ({ status: 201, body: await createUser(body.id, body.email) }),
 }));
